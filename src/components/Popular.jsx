dd848b75851ffe18c8c06dced7e08c5505bc5a22
matthew <matthewyt765@gmail.com>
2025-07-26 22:26:54 +0800

    margin

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,9 +3,9 @@
 export default function Popular() {
   return (
     <>
-      <hr className="border-8 border-primary" />
+      <hr className="border-5 border-primary" />
       <section className="w-screen flex justify-center bg-[#F5E1DA]/70">
-        <div className="container mt-7 ">
+        <div className="container my-7 ">
           <h1 className="text-primary text-5xl font-bold text-center">Popular Dish</h1>
           <div className="grid grid-cols-1 mx-auto my-4 gap-20 lg:grid-cols-3">
             <div className="h-60 rounded-2xl border-2 border-primary text-center text-wrap">
