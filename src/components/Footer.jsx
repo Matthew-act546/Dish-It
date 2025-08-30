c99272b960bd45938fcb99f8dd53568d3e9fad43
Matthew <matthewyt765@gmail.com>
2025-08-30 23:38:38 +0800

    fix the footers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,25 @@
+import { Link } from "react-router";
+
 export default function Footer() {
+  const navigation = [
+    { name: "Home", to: "/" },
+    { name: "Favorites", to: "/Favorites" },
+    { name: "Country Dish", to: "/CountryDish" },
+    { name: "Browse Dish", to: "/BrowseDish" },
+  ];
+
+  const webData = [
+    { name: "Github(source code)", href: "/" },
+    { name: "Figma Design", href: "#" },
+    { name: "Meal it API", href: "/CountryDish" },
+  ];
+
+  const portfolio = [
+    { name: "Github", href: "/" },
+    { name: "Facebook", href: "/Favorites" },
+    { name: "Upwork", href: "/CountryDish" },
+  ];
+
   return (
     <>
       <section className="flex justify-center bg-primary text-white">
@@ -10,25 +31,47 @@ export default function Footer() {
             </div>
             <div>
               <ul className="list-none">
-                <li><a className="hover:underline" href="#">Home</a></li>
-                <li><a className="hover:underline" href="#">Favorites</a></li>
-                <li><a className="hover:underline" href="#">Country Dish</a></li>
-                <li><a className="hover:underline" href="#">Browse Dish</a></li>
-                <li><a className="hover:underline" href="#">Popular Dish</a></li>
+                <li>
+                  {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.to}
+                    className={`hover:underline`}
+                  >
+                    {item.name} <br />
+                  </Link>
+                  ))}
+                </li>
               </ul>
             </div>
             <div>
               <ul className="list-none">
-                <li><a className="hover:underline" href="#">Github(source code)</a></li>
-                <li><a className="hover:underline" href="#">Figma Design</a></li>
-                <li><a className="hover:underline" href="#">Meal it API</a></li>
+                <li>
+                  {webData.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={`hover:underline`}
+                  >
+                    {item.name} <br />
+                  </Link>
+                  ))}
+                </li>
               </ul>
             </div>
             <div>
               <ul className="list-none">
-                <li><a className="hover:underline" href="#">Github</a></li>
-                <li><a className="hover:underline" href="#">Facebook</a></li>
-                <li><a className="hover:underline" href="#">Upwork</a></li>
+                <li>
+                  {portfolio.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={`hover:underline`}
+                  >
+                    {item.name} <br />
+                  </Link>
+                  ))}
+                </li>
               </ul>
             </div>
           </div>
