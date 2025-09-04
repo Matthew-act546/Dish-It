75a3a4d0fec810c0624c4125abc01a6bf92242e7
Matthew <matthewyt765@gmail.com>
2025-09-04 19:42:17 +0800

    war is over. i fucking fix it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,8 @@ export default function Home() {
     <>
       
       <NavigationalBar />
-      <section className="w-full h-screen md:[100dvh] bg-secondary/20 pb-10">
+      <section className="w-full min-h-screen bg-secondary/20 pb-10">
+
         <div className="banner bg-primary w-full">
           <div className="flex justify-center items-center py-7 lg:py-10 mx-7">
             <form className="flex max-w-lg w-full relative">
@@ -50,35 +51,33 @@ export default function Home() {
           </div>
         </div>
 
-      <div className="container my-5 lg:my-10 flex flex-col lg:flex-row items-center justify-between gap-4 lg:gap-10 mx-auto px-4">
-  
-        
-        <div className=" flex-1 min-w-[18rem]
-      text-xl rounded-3xl bg-primary/90 p-6 md:p-10 shadow-md shadow-primary text-white">
-          <h1 className="text-3xl md:text-4xl rt-1 font-bold leading-tight mb-4">
-            Your personal recipe companion
-          </h1>
-          <p className="leading-relaxed rp-1 text-gray-50">
-            Whether you’re craving something new or want to recreate a classic, Meal It makes it easy to find, learn, and enjoy meals from all cuisines — all in one place.
-          </p>
-          <br />
-          <Link 
-            type="button"
-            className="btn-accent py-3 px-4  h-[3rem]"
-            to={"/BrowseDish"}>
-            Browse dish
-          </Link>
+        <div className="container my-5 lg:my-10 flex flex-col lg:flex-row items-center justify-between gap-4 lg:gap-10 mx-auto px-4">
+    
+          
+          <div className=" flex-1 min-w-[18rem]text-xl rounded-3xl bg-primary/90 p-6 md:p-10 shadow-md shadow-primary text-white">
+            <h1 className="text-3xl md:text-4xl rt-1 font-bold leading-tight mb-4">
+              Your personal recipe companion
+            </h1>
+            <p className="leading-relaxed rp-1 text-gray-50">
+              Whether you’re craving something new or want to recreate a classic, Meal It makes it easy to find, learn, and enjoy meals from all cuisines — all in one place.
+            </p>
+            <br />
+            <Link 
+              type="button"
+              className="btn-accent py-3 px-4  h-[3rem]"
+              to={"/BrowseDish"}>
+              Browse dish
+            </Link>
+          </div>
+
+          <div className="w-full md:flex-1">
+            <img
+              src="dish1-h.jpeg"
+              alt="Delicious food"
+              className="rounded-3xl shadow-xl shadow-primary w-full h-full object-cover"
+            />
+          </div>
         </div>
-
-        <div className="md:h-[32rem] flex items-center w-full  md:flex-1">
-          <img src="dish1-h.jpeg" alt="Delicious food"
-            className="rounded-3xl md:h-[20rem] lg:h-[30rem] shadow-xl shadow-primary w-full " />
-        </div>
-
-      </div>
-
-
-
       </section>
     </>
   );
